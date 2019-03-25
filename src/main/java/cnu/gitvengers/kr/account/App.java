18e47ded48551e7306fe925ca0d5712f273206a2
DoDaek <dbxor8960@naver.com>
2019-03-25 18:13:40 +0900

    Start Project

---
+++ src/main/java/cnu/gitvengers/kr/account/App.java
@@ -0,0 +1,13 @@
+package cnu.gitvengers.kr.account;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
